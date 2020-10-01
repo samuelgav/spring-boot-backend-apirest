9e19a35108b960bf5c66170cd2007b1d14cd0c0b
USER <USER@DESKTOP-PP3EKGB>
2020-10-01 18:37:42 -0500

    g

---
+++ apirestpaises/src/main/java/com/developer/apirestpaises/ApirestpaisesApplication.java
@@ -0,0 +1,13 @@
+package com.developer.apirestpaises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestpaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestpaisesApplication.class, args);
+	}
+
+}
